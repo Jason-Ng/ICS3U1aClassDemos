3b832faf52d714430438fd181e2c3688fe05ac9b
Jason <Jason@10.130.178.129>
2017-09-15 14:11:49 -0400

    Added second message

--- src/HelloSync.java
+++ src/HelloSync.java
@@ -3,7 +3,11 @@ public class HelloSync {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// Add comment 
 		System.out.println("Hello from school");
+		
+		System.out.println("Yo yoo Carson, what's good fam?");
 	}
 
 }
